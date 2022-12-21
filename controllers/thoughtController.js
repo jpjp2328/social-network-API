f5b642894e58dabbb74021e6444b293c665d0931
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-21 23:49:26 +1100

    updated controllers with Todo comments

---
+++ controllers/thoughtController.js
@@ -0,0 +1,18 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all thoughts - getThoughts
+
+    // Get a single thought - getSingleThought
+
+    // Create a thought - createThought
+
+    // Update a thought - updateThought
+
+    // Delete a thought - deleteThought
+
+    // Create a reaction - createReaction
+
+    // Delete a reaction - deleteReaction
+    
+}
---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all users - getUsers
+
+    // Get a single user - getSingleUser
+
+    // Create a user - createUser
+
+    // Update a user - updateUser
+
+    // Delete a user - deleteUser
+
+    // Add a friend - addFriend
+
+    // Delete a friend - deleteFriend
+    
+}
