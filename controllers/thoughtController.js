e271ab100e1dfbdb81822ce3df2c6ae718ffadee
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-22 22:44:24 +1100

    create/delete reaction functions added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -57,10 +57,47 @@ module.exports = {
                     : res.json({ message: 'Thoughts deleted!' })
             )
             .catch((err) => res.status(500).json(err));
-    }
+    },
 
     // Create a reaction - createReaction
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            {
+                _id: req.params.thoughtId
+            },
+            {
+                $addToSet: { reactions: req.body }
+            },
+            {
+                new: true
+            },
+        )
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({ message: 'No thought with that ID' })
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 
     // Delete a reaction - deleteReaction
-
-}
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            {
+                _id: req.params.thoughtId
+            },
+            {
+                $pull: { reactions: req.params.reactionId }
+            },
+            {
+                new: true
+            },
+        )
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({ message: 'No thought with that ID' })
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+};
