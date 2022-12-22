223300668c312da2d88a11974b8a70552cb29e52
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-22 20:58:04 +1100

    added getUsers/getSingleUser in controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,23 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     // Get all users - getUsers
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
 
     // Get a single user - getSingleUser
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) => 
+                !user
+                   ? res.status(404).json({ message: 'No user with that ID'})
+                   : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 
     // Create a user - createUser
 
@@ -14,5 +29,5 @@ module.exports = {
     // Add a friend - addFriend
 
     // Delete a friend - deleteFriend
-    
+
 }
