da150fb1c12500d17c14cc1870caffd4bc076af3
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-12-22 22:08:48 +1100

    getThoughts/getSingleThought added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,8 +2,23 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     // Get all thoughts - getThoughts
+    getThoughts(req, res) {
+        Thought.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
 
     // Get a single thought - getSingleThought
+    getSingleThoughts(req, res) {
+        Thought.find({ _id: req.params.userId })
+        .select('__v')
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({ message: 'No thought with that ID' })
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 
     // Create a thought - createThought
 
